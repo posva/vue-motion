35d2dd3f027c0157e86fa6a7b0ec3730ab74963a
Eduardo San Martin Morote <posva13@gmail.com>
2017-04-06 20:54:32 +0200

    🚑 Forgot this demo file
    Signed-off-by: Eduardo San Martin Morote <posva13@gmail.com>

---
+++ docs/src/PhotosContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <Motion tag="div"
+          :value="left"
+  >
+    <template scope="props">
+      <slot :left="props.value"></slot>
+    </template>
+  </Motion>
+</template>
+
+<script>
+import Motion from '../../src/Motion'
+
+export default {
+  props: ['sizes', 'current'],
+
+  computed: {
+    left () {
+      let cumulated = 0
+      for (let i = 0; i < this.current; ++i) {
+        cumulated += this.sizes[i].width
+      }
+      return -cumulated
+    },
+  },
+
+  created () {
+    console.log('hey')
+  },
+
+  components: { Motion },
+}
+</script>
